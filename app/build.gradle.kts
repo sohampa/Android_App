8a48559a76ff4c0aaee7afdc4a86b1def47d148d
sohamparik <sohampa@cybage.com>
2023-09-04 12:27:44 +0530

    code updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ sonarqube {
     }
 }
 
+
 android {
     namespace = "com.example.myapplication"
     compileSdk = 33
@@ -68,7 +69,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("com.android.support:support-annotations:28.0.0")
+    implementation("androidx.annotation:annotation:1.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
